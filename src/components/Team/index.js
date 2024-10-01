1918c41d74ba100b1e897e559ea9bc1d31851458
gabizelaa <gbrandaomartins11@hotmail.com>
2024-10-01 19:21:29 -0300

    corrigindo index.js de times

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,18 +1,27 @@
-import Collaborator from '../Collaborator'
-import './Team.css'
+import Collaborator from "../Collaborator";
+import "./Team.css";
 
 const Team = (props) => {
-    const css = { backgroundColor: props.secondaryColor }
+  const css = { backgroundColor: props.secondaryColor };
 
-    return (
-        (props.collaborators.length > 0) ? <section className='team' style={css}>
-            <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
-            <div className='collaborators'>
-                {props.collaborators.map( collaborator => <collaborator backgroundColor={props.primaryColor} key={Collaborator.name} name={collaborator.name} position={collaborator.position} imagem={collaborator.image}/> )}
-            </div>
-        </section> 
-        : ''
-    )
-}
+  return props.collaborators.length > 0 ? (
+    <section className="team" style={css}>
+      <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+      <div className="collaborators">
+        {props.collaborators.map((collaborator) => (
+          <collaborator
+            backgroundColor={props.primaryColor}
+            key={Collaborator.name}
+            name={collaborator.name}
+            position={collaborator.position}
+            image={collaborator.image}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
 
 export default Team;
